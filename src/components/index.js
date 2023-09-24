4cecdce21d2c79c46821bd7cd690998ed018778e
utkarshsrivastav12 <theutkarsh5@gmail.com>
2023-09-24 19:51:54 +0530

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
-import Client from "./Client";
+import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
@@ -14,7 +14,7 @@ export {
   Billing,
   CardDeal,
   Business,
-  Client,
+  Clients,
   CTA,
   Stats,
   Footer,
